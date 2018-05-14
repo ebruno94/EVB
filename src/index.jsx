fa14c7127a7e23d176e7859d8cd4f54128611f94
Ernest <ernest.bruno94@yahoo.com>
2018-05-14 09:58:24 -0700

    Remove redux statements for the time being

--- src/index.jsx
+++ src/index.jsx
@@ -4,24 +4,11 @@ import App from './components/App';
 import { HashRouter } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
 
-// import {createStore} from 'redux';
-// import {Provider} from 'react-redux';
-//
-// import rootReducer from './reducers/index'
-//
-// const store = createStore(rootReducer);
-//
-// let unsubscribe = store.subscribe(() =>
-//   console.log(store.getState())
-// );
-
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
       <HashRouter>
-        // <Provider store={store}>
-          <Component/>
-        // </Provider>
+        <Component/>
       </HashRouter>
     </AppContainer> ,
     document.getElementById('react-app-root')
