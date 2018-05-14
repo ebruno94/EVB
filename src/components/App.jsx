71e0a07274a1ea1174cb9a87716ed27504780077
Ernest <ernest.bruno94@yahoo.com>
2018-05-14 10:51:03 -0700

    Add boilerplate code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,9 @@ import Header from './Header';
 import Footer from './Footer';
 import Error404 from './Error404';
 import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import Portfolio from './Portfolio';
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -17,6 +20,9 @@ export default class App extends React.Component{
         <Header/>
         <Switch>
           <Route exact path='/' render={()=><Home/>} />
+          <Route path='/portfolio' render={()=><Portfolio/>} />
+          <Route path='/about' render={()=><About/>} />
+          <Route path='/contact' render={()=><Contact/>} />
           <Route component={Error404}/>
         </Switch>
         <Footer/>
