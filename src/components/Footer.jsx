51ba8aa31489a21275986cda307ac4fd89b914bf
Ernest <ernest.bruno94@yahoo.com>
2018-05-14 10:07:21 -0700

    Fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,13 @@ export default function Footer(){
       <style jsx>{`
         h1{
           text-align: center;
+          bottom: 0;
+          left: 0;
+          position: fixed;
+          background-color: gray;
+          width: 100%;
+          z-index: 1;
+          margin-bottom: 0px;
         }
       `}</style>
       <h1>Copyright (c) 2018</h1>
