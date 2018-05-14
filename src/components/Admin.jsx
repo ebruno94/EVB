025419f87bb3c061034e16aed5c654dbbfc45c7c
Ernest <ernest.bruno94@yahoo.com>
2018-05-14 16:39:25 -0700

    Add AdminLogin component

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import AdminLogin from './AdminLogin';
 
 export default function Admin(){
   return(
     <div>
       <h1>THIS IS THE ADMIN PAGE</h1>
+      <AdminLogin/>
     </div>
   );
 }
