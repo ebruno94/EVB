55e47b5190f04d19d309090ebde22bb0e6edcdd1
Ernest <ernest.bruno94@yahoo.com>
2018-05-14 13:32:13 -0700

    Update about page: now includes buttons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 
-export default function About(){
-  return(
-    <div>
+export default class About extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      clicked: 'who'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  };
+
+  handleClick(component){
+    this.setState({clicked: component};);
+  };
+
+  render(){
+    return(
       <div>
-        <h1>This is the About page</h1>
+        <h1>ABOUT US</h1>
+        <button onClick={() => this.handleClick('who')}>Who We Are</button>
+        <button onClick={() => this.handleClick('mission')}>Mission</button>
+        <button onClick={() => this.handleClick('services')}>Services</button>
+        <div>
+          <h1>WHO WE ARE</h1>
+          <hr/>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <br/>
+
+          <h1>MISSION</h1>
+          <hr/>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+          <h1>SERVICES</h1>
+          <hr/>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
