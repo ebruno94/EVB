7a125bfc4cc6fa605925e0c551779faff5ee08ed
Ernest <ernest.bruno94@yahoo.com>
2018-05-15 09:00:25 -0700

    Access passed masterProjectList prop and create a map statement to display all projects

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Project from './Project';
+import PropTypes from 'prop-types';
 
-export default function ProjectList(){
+export default function ProjectList(props){
   return(
     <div>
       <h1>Project List</h1>
-      <Project/>
+      {Object.keys(props.projectList).map(function(projectId){
+        let project = props.projectList[projectId];
+        return <Project
+          location={project.location}
+          date={project.date}
+          images={project.images}
+          key={projectId}
+          projectId={projectId}/>;
+      })}
     </div>
   );
 }
+
+ProjectList.propTypes = {
+  projectList: PropTypes.object
+};
