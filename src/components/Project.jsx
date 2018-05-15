74e058b7ec14e09ddb766705dbf3dd9e4882a126
Ernest <ernest.bruno94@yahoo.com>
2018-05-15 09:01:08 -0700

    Access passed project prop and display necessary information about said project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Project(){
+export default function Project(props){
   return(
     <div>
-      <h1>Project goes here</h1>
+      <h1>{props.location} - {props.date}</h1>
     </div>
   );
 }
+
+Project.propTypes = {
+  location: PropTypes.string,
+  date: PropTypes.string,
+  images: PropTypes.array,
+  projectId: PropTypes.string
+};
