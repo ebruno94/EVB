146cf35f7c68f8c51f7ada8360463c9d83bd1a48
Ernest <ernest.bruno94@yahoo.com>
2018-05-15 13:55:17 -0700

    Create component that lists all the inquiries

---
+++ src/components/InquiryList.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Inquiry from './Inquiry';
+
+export default function InquiryList(props){
+  return(
+    <div>
+      <h1>Inquiries</h1>
+      <hr/>
+      {Object.keys(props.inquiryList).map(function(inquiryId){
+        let inquiry = props.inquiryList[inquiryId];
+        return <Inqury
+          name={inquiry.name}
+          phone={inquiry.phone}
+          email={inquiry.email}
+          question={inquiry.question}
+          key={inquiryId}
+          inquiryId={inquiryId} />
+      })}
+    </div>
+  );
+}
+
+InquiryList.propTypes = {
+  InquiryList: PropTypes.object
+};
