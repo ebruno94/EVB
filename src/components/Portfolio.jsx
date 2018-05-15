92e1f4a671ff048e1c60346b988455821de39d57
Ernest <ernest.bruno94@yahoo.com>
2018-05-15 08:58:09 -0700

    Edit Portfolio component to display necessary components such as the ProjectList and the Project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ProjectList from './ProjectList';
+import PropTypes from 'prop-types';
 
-export default function Portfolio(){
+export default function Portfolio(props){
   return(
     <div>
       <h1>Portfolio</h1>
       <hr/>
       <p>Below is a list of projects we have done in the past</p>
-      <ProjectList/>
+      <ProjectList projectList={props.projectList}/>
     </div>
   );
+}
+
+Portfolio.propTypes = {
+  projectList: PropTypes.object
 }
