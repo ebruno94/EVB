776db9447906c988ea59f289c31e90010d6bb82c
Ernest <ernest.bruno94@yahoo.com>
2018-05-15 10:28:14 -0700

    Add ternary condition that makes log out button appear if admin login is successful

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default function Header(){
+export default function Header(props){
+
   return(
     <div>
       <h1>EVB</h1>
       <h1>Construction and Trading Services</h1>
-      <Link to='/'>Home</Link> | <Link to='/about'>About Us</Link> | <Link to='/portfolio'> Portfolio</Link> | <Link to='/contact'>Contact Us!</Link>
+      <Link to='/'>Home</Link> | <Link to='/about'>About Us</Link> | <Link to='/portfolio'> Portfolio</Link> | <Link to='/contact'>Contact Us!</Link> {(props.adminLoggedIn)?<button type='button' onClick={() => props.onAdminLogout(false)}>Logout</button> : <span></span>}
     </div>
   );
 }
+
+Header.propTypes = {
+  onAdminLogout: PropTypes.func,
+  adminLoggedIn: PropTypes.bool
+};
