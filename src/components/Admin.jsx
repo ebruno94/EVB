9b4b3672a015536d20d1559c167fedf8701a21d6
Ernest <ernest.bruno94@yahoo.com>
2018-05-15 10:30:38 -0700

    Add conditional statements that alter the way the page displays if admin is logged in or not:

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import AdminLogin from './AdminLogin';
+import PropTypes from 'prop-types';
 
-export default function Admin(){
-  return(
-    <div>
-      <h1>THIS IS THE ADMIN PAGE</h1>
-      <AdminLogin/>
-    </div>
-  );
+export default function Admin(props){
+  if (!props.adminLoggedIn){
+    return (
+      <div>
+        <h1>THIS IS THE ADMIN PAGE</h1>
+        <AdminLogin onAdminLogin={props.onAdminLogin}/>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>GOOD JOB</h1>
+      </div>
+    );
+  }
 }
+
+Admin.propTypes = {
+  onAdminLogin: PropTypes.func,
+  adminLoggedIn: PropTypes.bool
+};
