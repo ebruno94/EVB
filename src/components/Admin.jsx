f7c0ffa9596ddd38c759e57d5ffe00fa7a300ea2
Ernest <ernest.bruno94@yahoo.com>
2018-05-15 15:33:34 -0700

    Modify page to display inquiries

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import AdminLogin from './AdminLogin';
 import PropTypes from 'prop-types';
+import InquiryList from './InquiryList';
 
 export default function Admin(props){
   if (!props.adminLoggedIn){
     return (
       <div>
-        <h1>THIS IS THE ADMIN PAGE</h1>
+        <h1>Please Log in</h1>
         <AdminLogin onAdminLogin={props.onAdminLogin}/>
       </div>
     );
   } else {
     return (
       <div>
-        <h1>GOOD JOB</h1>
+        <h1>Welcome, Admin!</h1>
+        <InquiryList inquiryList={props.inquiryList}/>
       </div>
     );
   }
