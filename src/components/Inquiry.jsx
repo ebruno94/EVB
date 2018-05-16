950135fbcdedf1ec94aa48bc6e3663c407447626
Ernest <ernest.bruno94@yahoo.com>
2018-05-16 10:19:53 -0700

    Add handleResolve function that deletes selected inquiry

--- src/components/Inquiry.jsx
+++ src/components/Inquiry.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux'
 
-export default function Inquiry(props){
+function Inquiry(props){
+
+  function handleResolve(){
+    console.log(props);
+    const {dispatch} = props;
+    const action = {
+      type: 'REMOVE_INQUIRY',
+      id: props.inquiryId
+    };
+    if (confirm('Resolve this inquiry? (This action will delete the inquiry!)')){
+      dispatch(action);
+    };
+  }
+
   return(
     <div>
       <h1>Name: {props.name}</h1>
       <h3>E-mail: {props.email}</h3>
       <h4>Phone: {props.phone}</h4>
       <p>Question: {props.question}</p>
-      <button type='button'>Resolve</button>
+      <button onClick={handleResolve} type='button'>Resolve</button>
+      <hr/>
     </div>
   );
 }
@@ -18,4 +33,7 @@ Inquiry.propTypes = {
   email: PropTypes.string,
   phone: PropTypes.string,
   question: PropTypes.string,
+  inquiryId: PropTypes.string
 };
+
+export default connect()(Inquiry);
