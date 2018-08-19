c4fe74a4f0b5f8dc4b261c0e3ff5f557d77ed643
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-08-19 13:35:02 -0700

    Add styles to the page and add link to products page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+
 export default function Home(){
   return(
     <div>
@@ -10,6 +11,13 @@ export default function Home(){
           grid-template-columns: repeat(4, 1fr);
           grid-column-gap: 10px;
           justify-items: center;
+        }
+
+        .container {
+          display: block;
+          margin-left: 100px;
+          margin-right: 100px;
+          padding: 20px;
         }
 
         .box{
@@ -24,35 +32,46 @@ export default function Home(){
 
         h1{
           margin-bottom: 0px;
+          text-align: center;
+        }
+
+        img {
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+          height: 300px;
+          width: 450px;
         }
 
       `}</style>
-      <h1>LOGO WILL BE IN HERE</h1>
-      <h1>WELCOME</h1>
-      <hr/>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <hr/>
-      <div className='pages'>
-        <Link style={{textDecoration: 'none', color: 'black'}} to='/about'>
-          <div className='box'>
-            <h1>About Us</h1>
-          </div>
-        </Link>
-        <Link style={{textDecoration: 'none', color: 'black'}} to='/portfolio'>
-          <div className='box'>
-            <h1>Portfolio</h1>
-          </div>
-        </Link>
-        <Link style={{textDecoration: 'none', color: 'black'}} to='/contact'>
-          <div className='box'>
-            <h1>Contact Us</h1>
-          </div>
-        </Link>
-        <Link style={{textDecoration: 'none', color: 'black'}} to='/admin'>
-          <div className='box'>
-            <h1>Admin Login</h1>
-          </div>
-        </Link>
+      <img src={require('../assets/img/EVB-logo-trans.png')}></img>
+      <div className='container'>
+        <h1>WELCOME</h1>
+        <hr/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <hr/>
+        <div className='pages'>
+          <Link style={{textDecoration: 'none', color: 'black'}} to='/about'>
+            <div className='box'>
+              <h1>About Us</h1>
+            </div>
+          </Link>
+          <Link style={{textDecoration: 'none', color: 'black'}} to='/products'>
+            <div className='box'>
+              <h1>Products</h1>
+            </div>
+          </Link>
+          <Link style={{textDecoration: 'none', color: 'black'}} to='/contact'>
+            <div className='box'>
+              <h1>Contact Us</h1>
+            </div>
+          </Link>
+          <Link style={{textDecoration: 'none', color: 'black'}} to='/admin'>
+            <div className='box'>
+              <h1>Admin Login</h1>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
