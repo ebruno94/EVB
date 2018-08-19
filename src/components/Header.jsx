42f2329f3441bde99c5741652a1e3da9eebab451
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-08-19 13:51:51 -0700

    Add the logo and link to products

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,40 @@ import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export default function Header(props){
+  let adminLinkToggle;
+  if (props.adminLoggedIn){
+    adminLinkToggle = <div> <Link to='/admin'>Admin Page</Link> | <button type='button' onClick={() => props.onAdminLogout(false)}> Logout </button> </div>
+  } else {
+    adminLinkToggle = <Link to='/admin'>Admin Login</Link>
+  }
 
   return(
     <div>
-      <h1>EVB</h1>
-      <h1>Construction and Trading Services</h1>
-      <Link to='/'>Home</Link> | <Link to='/about'>About Us</Link> | <Link to='/portfolio'> Portfolio</Link> | <Link to='/contact'>Contact Us!</Link> {(props.adminLoggedIn)?
-        <div> <Link to='/admin'>Admin Page</Link><button type='button' onClick={() => props.onAdminLogout(false)}>Logout</button>
-        </div>  : <Link to='/admin'>Admin Login</Link>}
+      <style jsx>{`
+          h1, .links {
+            text-align: center;
+          }
+
+          img {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            height: 200px;
+            width: 350px;
+            margin-bottom: 25px;
+          }
+
+          .headerContainer {
+            display: block;
+            margin-bottom: 25px;
+          }
+      `}</style>
+    <div className='headerContainer'>
+        <img src={require('../assets/img/EVB-logo-trans.png')}></img>
+        <div className='links'>
+          <Link to='/'>Home</Link> | <Link to='/products'>Products</Link> | <Link to='/about'>About Us</Link> | <Link to='/portfolio'> Portfolio</Link> | <Link to='/contact'>Contact Us!</Link> | {adminLinkToggle}
+        </div>
+      </div>
     </div>
   );
 }
