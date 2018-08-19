9dd6dff6b1f2a79cfb6fa75e90b2d12815a1d82a
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-08-19 13:19:23 -0700

    Create functions that take care of connections to the firebase database

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ import Firebase from 'firebase';
 
 Firebase.initializeApp(firebaseConfig);
 const inquiries = Firebase.database().ref('inquiries');
+const items = Firebase.database().ref('items');
 
 export function addInquiry(_name, _email, _phone, _question){
   return () => inquiries.push({
@@ -32,19 +33,60 @@ export function watchFirebaseInquiriesRef(){
       });
       dispatch(deleteInquiry(inquiryToBeDeleted));
     });
+
   };
 }
 
-function receiveInquiry(firebaseInquiry){
+export function receiveInquiry(firebaseInquiry){
   return {
     type: 'RECEIVE_INQUIRY',
     inquiry: firebaseInquiry
   };
 }
 
-function deleteInquiry(firebaseInquiry){
+export function deleteInquiry(firebaseInquiry){
   return {
     type: 'REMOVE_INQUIRY',
     id: firebaseInquiry.id
   };
+}
+
+// Items
+export function addItem(_name, _imageUrl, _quantity){
+  return () => items.push({
+    name: _name,
+    image: _imageUrl,
+    quantity: _quantity
+  });
+}
+
+export function receiveItem(firebaseItem){
+  return {
+    type: 'RECEIVE_ITEM',
+    item: firebaseItem
+  };
+}
+
+export function deleteItem(firebaseItem){
+  return {
+    type: 'REMOVE_ITEM',
+    id: firebaseItem.id
+  };
+}
+
+export function watchFirebaseItemsRef(){
+  return (dispatch) => {
+    items.on('child_added', data => {
+      const newItem = Object.assign({}, data.val(), {
+        id: data.getKey()
+      });
+      dispatch(receiveItem(newItem));
+    });
+    inquiries.on('child_removed', data =>{
+      const itemToBeDeleted = Object.assign({}, data.val(), {
+        id: data.getKey()
+      });
+      dispatch(deleteItem(itemToBeDeleted));
+    });
+  }
 }
