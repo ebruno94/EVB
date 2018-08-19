0affc677393cbb2bd33792a6839f77d95c16aa4d
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-08-19 13:25:59 -0700

    Modify some styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,11 +30,58 @@ export default class About extends React.Component{
   render(){
     return(
       <div>
-        <h1>ABOUT US</h1>
-        <button onClick={() => this.handleClick('who')}>Who We Are</button>
-        <button onClick={() => this.handleClick('mission')}>Mission</button>
-        <button onClick={() => this.handleClick('services')}>Services</button>
-        {this.handleRenderComponent()}
+        <style jsx global>{`
+            h1 {
+              text-align: center;
+            }
+
+            button {
+              border: 2px solid black;
+              color: black;
+              padding: 15px 32px;
+              text-align: center;
+              text-decoration: none;
+              display: inline-block;
+              font-size: 16px;
+            }
+
+            button:hover{
+              
+            }
+
+            .buttonContainer {
+              display: grid;
+              grid-template-columns: repeat(3, 1fr);
+              grid-column-gap: 100px;
+              justify-items: center;
+              margin-left: 100px;
+              margin-right: 100px;
+              margin-top: 25px;
+              margin-bottom: 25px;
+            }
+
+            .container {
+              display: block;
+              margin-left: 100px;
+              margin-right: 100px;
+              border: 2px solid black;
+              padding: 20px;
+              border-radius: 25px;
+            }
+
+
+        `}</style>
+        <div>
+          <h1>ABOUT US</h1>
+          <hr/>
+          <div className='buttonContainer'>
+            <button onClick={() => this.handleClick('who')}>Who We Are</button>
+            <button onClick={() => this.handleClick('mission')}>Mission</button>
+            <button onClick={() => this.handleClick('services')}>Services</button>
+          </div>
+          <hr/>
+          {this.handleRenderComponent()}
+        </div>
       </div>
     );
   }
